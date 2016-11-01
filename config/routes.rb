3cec4ca0f88b2a8ec0abeff331e3eb03ea2c6158
PMatsis <pieros.matsis@users.noreply.github.com>
2016-10-31 23:40:44 -0600

    Building_CRUD
    CRUD-photogram

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,21 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
+  #Routes to CREATE photos
+  get("/photos/new",     { :controller => "photos", :action => "new_form" })
+  get("/create_photo",     { :controller => "photos", :action => "create_row" })
+
+  #Routes to DELETE photos
+  get("/delete_photo/:id",     { :controller => "photos", :action => "delete_row" })
+
+
+  #Routes to EDIT photos
+  get("/photos/:id/edit", { :controller => "photos", :action => "edit_form" })
+
   # Routes to READ photos
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+
 
 end
